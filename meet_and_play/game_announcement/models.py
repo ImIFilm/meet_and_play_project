2b69d796b31048be77f89a5b9a73c14d565996d0
jkalfas <jkalfas179@gmail.com>
2019-04-06 11:26:25 +0200

    Add fixed values for sports and skill

--- meet_and_play/game_announcement/models.py
+++ meet_and_play/game_announcement/models.py
@@ -1,16 +1,29 @@
 from django.db import models
 from datetime import datetime 
+from enum import Enum
+
+class SkillLevel(Enum):
+	REC = "Recreational"
+	AMA = "Amatuer"
+	AVG = "Average"
+	GOO = "Good"
+	VGO = "Very Good"
+
+class Sports(Enum):
+	FOT = "Football"
+	BAS = "Basketball"
+	VOL = "Volleyball"
 
 class Game_Announcment(models.Model):
 	""" doc to do"""
-	sport = models.CharField("Sport", default="Sport",max_length=100)
+	sport = models.CharField("Sport", max_length=100, choices=[(tag, tag.value) for tag in Sports])
 	location = models.CharField("Location", default="Location",max_length=100)
 	start_time = models.DateTimeField(default=datetime.now)
 	end_time = models.DateTimeField(default=datetime.now)
 	wanted_people = models.IntegerField()
 	registered_people = models.IntegerField()
 	price = models.FloatField(default=0)
-	skill_level = models.CharField("Level of skills", default="Average", max_length=100)
+	skill_level = models.CharField("Level of skills", max_length=100, choices=[(tag, tag.value) for tag in SkillLevel])
 	description = models.TextField("Description")
 
 	
